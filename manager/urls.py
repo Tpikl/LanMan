a7f58f40a0df230eb9558237b4678c18bce43e0d
Tayler Pickles <pickles.tayler@gmail.com>
2016-01-24 14:25:51 -0700

    -Small changes to migrations -Add general views

--- manager/urls.py
+++ manager/urls.py
@@ -3,5 +3,12 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+    #ex: /manager/
     url(r'^$', views.index, name='index'),
+    #ex: /manager/5/
+    url(r'^(?P<questionID>[0-9]+)/$', views.detail, name='index'),
+    #ex: /manager/5/results/
+    url(r'^(?P<questionID>[0-9]+)/results/$', views.results, name='results'),
+    #ex: /manager/5/vote/
+    url(r'^(?P<questionID>[0-9]+)/vote/$', views.vote, name='vote'),
 ]
--- manager/views.py
+++ manager/views.py
@@ -4,3 +4,15 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse("Hello world! You're at the manager index.")
+
+def detail(request, questionID):
+    return HttpResponse("You're looking at question %s." % questionID)
+
+def results(request, questionID):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % questionID)
+
+def vote(request, questionID):
+    return HttpResponse("You're voting on question %s." % questionID)
+
+
