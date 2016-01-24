56bc84ff1f246b23ab7ab6eaa33d45f2dc9c481b
Tayler Pickles <pickles.tayler@gmail.com>
2016-01-24 05:47:16 -0700

    -Small name adjustment

--- lanMan/wsgi.py
+++ lanMan/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for lanMan project.
+WSGI config for LanMan project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "lanMan.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "LanMan.settings")
 
 application = get_wsgi_application()
